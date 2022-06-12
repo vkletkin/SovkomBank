093319562a0e0321368fe85e6ba4faa171f9e4a3
Vasiliy Kletkin <v.kletkin@mail.ru>
2022-06-12 17:28:13 +0300

    ""

--- users/views.py
+++ users/views.py
@@ -2,8 +2,9 @@ from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CreationForm
 
-
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy("login") #  где login — это параметр "name" в path()
     template_name = "signup.html"
+
+    
